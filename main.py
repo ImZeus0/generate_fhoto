364835e119590511f6486228e03146eb3d36b4e0
ImZeus0 <zeus1zeus101@gmail.com>
2022-05-05 13:11:19 +0300

    rg

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from os import path
 
 
 def create_dirs(finish_dir,name):
-    os.mkdir(name)
+    res_path = path.join(finish_dir,name)
+    if path.exists(res_path) == False:
+        os.mkdir(res_path)
+        os.mkdir(path.join(res_path,'mipmap-hdpi'))
+        os.mkdir(path.join(res_path, 'mipmap-mdpi'))
+        os.mkdir(path.join(res_path, 'mipmap-xhdpi'))
+        os.mkdir(path.join(res_path, 'mipmap-xxhdpi'))
+        os.mkdir(path.join(res_path, 'mipmap-xxxhdpi'))
+    return res_path
+
+def create_hdpi(main_path,photo_path):
+    img = Image.open(photo_path)
+    ic_launcher = img.resize((72,72))
+
 
 
 
@@ -13,10 +26,10 @@ def create_dirs(finish_dir,name):
 
 def main(photo_dir,finish_dir):
     name = photo_dir.split('/')[-1].split('.')[0]
-    create_dirs(finish_dir,name)
+    main_path = create_dirs(finish_dir,name)
 
 
 if __name__ == "__main__":
-    FINISH_DIR = 'result'
+    FINISH_DIR = '/root/PycharmProjects/generate_fhoto/result'
     PHOTO_DIR = '/252098.jpg'
     main(PHOTO_DIR,FINISH_DIR)
